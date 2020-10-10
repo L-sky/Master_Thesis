ad79ba85b715d2b17b82cae4866d93a001b4a9b6
L-sky <lapchevsky.k@gmail.com>
2020-10-11 00:31:26 +0300

    updated setup to work with newer pytorch

--- setup_helper.py
+++ setup_helper.py
@@ -1,8 +1,8 @@
 from torch.utils.cpp_extension import BuildExtension
-from torch.utils.cpp_extension import BUILD_NAMEDTENSOR, COMMON_NVCC_FLAGS, COMMON_MSVC_FLAGS
+from torch.utils.cpp_extension import COMMON_NVCC_FLAGS
 from torch.utils.cpp_extension import _is_cuda_file, _join_cuda_home, _get_cuda_arch_flags
 import copy
-import re
+
 from setuptools.command.build_ext import build_ext
 
 
@@ -15,8 +15,6 @@ class BuildRDCExtension(BuildExtension):
         self._check_abi()
         for extension in self.extensions:
             self._add_compile_flag(extension, '-DTORCH_API_INCLUDE_EXTENSION_H')
-            if BUILD_NAMEDTENSOR:
-                self._add_compile_flag(extension, '-DBUILD_NAMEDTENSOR')
             self._define_torch_extension_name(extension)
             self._add_gnu_cpp_abi_flag(extension)
 
@@ -26,7 +24,6 @@ class BuildRDCExtension(BuildExtension):
         if self.compiler.compiler_type == 'msvc':
             self.compiler._cpp_extensions += ['.cu', '.cuh']
             original_compile = self.compiler.compile
-            original_spawn = self.compiler.spawn
         else:
             original_compile = self.compiler._compile
 
@@ -71,76 +68,5 @@ class BuildRDCExtension(BuildExtension):
                 # Put the original compiler back in place.
                 self.compiler.set_executable('compiler_so', original_compiler)
 
-        # TODO: fix this for separate compilation maybe someday
-        def win_wrap_compile(sources,
-                             output_dir=None,
-                             macros=None,
-                             include_dirs=None,
-                             debug=0,
-                             extra_preargs=None,
-                             extra_postargs=None,
-                             depends=None):
-
-            self.cflags = copy.deepcopy(extra_postargs)
-            extra_postargs = None
-
-            def spawn(cmd):
-                # Using regex to match src, obj and include files
-                src_regex = re.compile('/T(p|c)(.*)')
-                src_list = [
-                    m.group(2) for m in (src_regex.match(elem) for elem in cmd)
-                    if m
-                ]
-
-                obj_regex = re.compile('/Fo(.*)')
-                obj_list = [
-                    m.group(1) for m in (obj_regex.match(elem) for elem in cmd)
-                    if m
-                ]
-
-                include_regex = re.compile(r'((\-|\/)I.*)')
-                include_list = [
-                    m.group(1)
-                    for m in (include_regex.match(elem) for elem in cmd) if m
-                ]
-
-                if len(src_list) >= 1 and len(obj_list) >= 1:
-                    src = src_list[0]
-                    obj = obj_list[0]
-                    if _is_cuda_file(src):
-                        nvcc = _join_cuda_home('bin', 'nvcc')
-                        if isinstance(self.cflags, dict):
-                            cflags = self.cflags['nvcc']
-                        elif isinstance(self.cflags, list):
-                            cflags = self.cflags
-                        else:
-                            cflags = []
-
-                        cflags = COMMON_NVCC_FLAGS + cflags + _get_cuda_arch_flags(cflags)
-                        for flag in COMMON_MSVC_FLAGS:
-                            cflags = ['-Xcompiler', flag] + cflags
-                        cmd = [nvcc, '-c', src, '-o', obj] + include_list + cflags
-                    elif isinstance(self.cflags, dict):
-                        cflags = COMMON_MSVC_FLAGS + self.cflags['cxx']
-                        cmd += cflags
-                    elif isinstance(self.cflags, list):
-                        cflags = COMMON_MSVC_FLAGS + self.cflags
-                        cmd += cflags
-
-                return original_spawn(cmd)
-
-            try:
-                self.compiler.spawn = spawn
-                return original_compile(sources, output_dir, macros,
-                                        include_dirs, debug, extra_preargs,
-                                        extra_postargs, depends)
-            finally:
-                self.compiler.spawn = original_spawn
-
-        # Monkey-patch the _compile method.
-        if self.compiler.compiler_type == 'msvc':
-            self.compiler.compile = win_wrap_compile
-        else:
-            self.compiler._compile = unix_wrap_compile
-
+        self.compiler._compile = unix_wrap_compile
         build_ext.build_extensions(self)
